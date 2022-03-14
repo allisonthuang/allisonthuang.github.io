443ff17116e71a922900d0b2aa9d81e449a78158
Allison Huang <89219200+allisonthuang@users.noreply.github.com>
2022-03-14 15:49:13 +0000

    added actual menus

--- week0/tt0.py
+++ week0/tt0.py
@@ -1,97 +1,115 @@
-"""
-Introduction to Console Programming
-Writing a function to print a menu
-"""
+# menuy.py - function style menu
+# Imports typically listed at top
+# each import enables us to use logic that has been abstracted to other files and folders
 
 
-# Menu options in print statement
-def print_menu1():
-    print('1 -- Stringy' )
-    print('2 -- Numby' )
-    print('3 -- Matrix' )
-    print('4 -- Swap' )
-    print('5 -- Exit' )
-    runOptions()
+# Main list of [Prompts, Actions]
+# Two styles are supported to execute abstracted logic
+# 1. file names will be run by exec(open("filename.py").read())
+# 2. function references will be executed directly file.function()
+
+import test
+
+main_menu = [
+    ["Swap", test.swap],
+    ["Matrix", test.matrix],
+]
+
+# Submenu list of [Prompt, Action]
+# Works similarly to main_menu
+sub_menu = [
+    ["Ship Animation", test.ship],
+    ["Face", test.face],
+    ["Christmas Tree", test.tree],
+    ["Better Christmas Tree", test.driver],
+]
+
+patterns_sub_menu = [
+    ["Patterns", None],
+    ["PreFuncy", None],
+    ["Funcy", None],
+]
+
+# Menu banner is typically defined by menu owner
+border = "=" * 25
+banner = f"\n{border}\nPlease Select An Option\n{border}"
 
 
-# Menu options as a dictionary
-menu_options = {
-    1: 'Stringy',
-    2: 'Numby',
-    3: 'Matrix',
-    4: 'Swap',
-    5: 'Exit',
-}
+# def patterns_submenuc
+# using patterns_sub_menu list:
+# patterns_submenuc works similarly to menuc
+def patterns_submenuc():
+    title = "Class Submenu" + banner
+    m = questy.Menu(title, patterns_sub_menu)
+    m.menu()
 
-# Print menu options from dictionary key/value pair
-def print_menu2():
-    for key in menu_options.keys():
-        print(key, '--', menu_options[key] )
-    runOptions()
 
-# menu option 1
-def stringy():
-    print('You chose \' 1 -  Stringy\'')
+# def menu
+# using main_menu list:
+# 1. main menu and submenu reference are created [Prompts, Actions]
+# 2. menu_list is sent as parameter to menuy.menu function that has logic for menu control
+def menu():
+    title = "Function Menu" + banner
+    menu_list = main_menu.copy()
+    menu_list.append(["Animations", submenu])
+    # menu_list.append(["Patterns", patterns_submenu])
+    buildMenu(title, menu_list)
 
-# menu option 2
-def numby():
-    print('You chose \' 2 - Numby\'')
+# def submenu
+# using sub menu list above:
+# sub_menu works similarly to menu()
+def submenu():
+    title = "Function Submenu" + banner
+    buildMenu(title, sub_menu)
+def patterns_submenu():
+    title = "Function Submenu" + banner
+    buildMenu(title, patterns_sub_menu)
 
-# menu option 3
-def matrix():
-  matrixa = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-  matrixb = []
-  matrix1(matrixa, matrixb)
-  matrix2(matrixb)
-  
-def matrix1(matrixa, matrixb):
-  for i in range(len(matrixa)):
-      for j in range(len(matrixa[i])):
-          matrixb.append(matrixa[i][j])
-  return(matrixb)
+def buildMenu(banner, options):
+    # header for menu
+    print(banner)
+    # build a dictionary from options
+    prompts = {0: ["Exit", None]}
+    for op in options:
+        index = len(prompts)
+        prompts[index] = op
 
-def matrix2(matrixa, matrixb):
-  print(matrixb[0], matrixb[1], matrixb[2])
-  print(matrixb[3], matrixb[4], matrixb[5])
-  print(matrixb[6], matrixb[7], matrixb[8])
-  
-# menu option 4
-def swap():
-  c = int(input('Whats your first number?:'))
-  d = int(input('Whats your second number?:'))
-  c,d = swap1(c,d)
-  print(c,d) 
+    # print menu or dictionary
+    for key, value in prompts.items():
+        print(key, '->', value[0])
 
-def swap1(a,b):
-  if a > b:
-    b, a = a, b  # swap values
-  else:
-    a, b = a, b
-  return a, b  # return 2 values
+    # get user choice
+    choice = input("Type your choice> ")
 
-# call functions based on input choice
-def runOptions():
-    # infinite loop to accept/process user menu choice
-    while True:
+    # validate choice and run
+    # execute selection
+    # convert to number
+    try:
+        choice = int(choice)
+        if choice == 0:
+            # stop
+            return
         try:
-            option = int(input('Enter your choice 1-5: '))
-            if option == 1:
-                stringy()
-            elif option == 2:
-                numby()
-            elif option == 3:
-                matrix()
-            elif option == 4:
-                swap()
-            # Exit menu    
-            elif option == 5:  
-                print('Exiting! Thank you! Good Bye...')
-                exit() # exit out of the (infinite) while loop
-            else:
-                print('Invalid option. Please enter a number between 1 and 5.')
-        except ValueError:
-            print('Invalid input. Please enter an integer input.')
+            # try as function
+            action = prompts.get(choice)[1]
+            action()
+        except TypeError:
+            try:  # try as playground style
+                exec(open(action).read())
+            except FileNotFoundError:
+                print(f"File not found!: {action}")
+            # end function try
+        # end prompts try
+    except ValueError:
+        # not a number error
+        print(f"Not a number: {choice}")
+    except UnboundLocalError:
+        # traps all other errors
+        print(f"Invalid choice: {choice}")
+    # end validation try
 
-if __name__=='__main__':
-    # print_menu1()
-    print_menu2()
+    buildMenu(banner, options)  # recursion, start menu over again
+
+
+if __name__ == "__main__":
+    menu()
